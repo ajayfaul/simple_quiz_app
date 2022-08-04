054d2ee349f2c711af1d3601a29b477fd7ab89fa
Muhammad Fajar <muhammadfajarm@gmail.com>
2022-08-04 20:07:46 +0700

    edit more on script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//selecting all required elements
+//set required elements
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
@@ -12,18 +12,18 @@ const timeCount = document.querySelector(".timer .timer_sec");
 
 // if startQuiz button clicked
 start_btn.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show info box
+    info_box.classList.add("activeInfo"); 
 }
 
 // if exitQuiz button clicked
 exit_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
+    info_box.classList.remove("activeInfo"); 
 }
 
 // if continueQuiz button clicked
 continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.add("activeQuiz"); //show quiz box
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.add("activeQuiz");
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
     startTimer(30); //calling startTimer function
