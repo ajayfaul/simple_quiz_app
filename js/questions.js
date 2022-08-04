1d75659568e4e3648d295c7353920b2e7d862ac9
Muhammad Fajar <muhammadfajarm@gmail.com>
2022-08-04 19:37:20 +0700

    add questions to questions.js

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The solar system formed from a cloud of fog is a theory:",
+    answer: "Nebula",
+    options: [
+      "Big Bang",
+      "Planetesimal",
+      "Twin Star",
+      "Nebula"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is a planet that lies beyond the orbit of the planet Earth called?",
+    answer: "Superior",
+    options: [
+      "In",
+      "Superior",
+      "Outside",
+      "Inferior"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The inner planet group is?",
+    answer: "Mercury, Venus, Earth",
+    options: [
+      "Mercury, Venus, Earth",
+      "Earth, Mars, Jupiter",
+      "venus, Earth, Mars",
+      "Mars, Jupiter, Saturn"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Model of the solar system which states that the earth is the center of the solar system?",
+    answer: "Geosentris",
+    options: [
+      "Heliosentris",
+      "Koosentris",
+      "Geosentris",
+      "Mediusentris"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Keepler's third law is called law?",
+    answer: "Harmonic",
+    options: [
+      "Harmonic",
+      "Broad Similarity",
+      "Elipse",
+      "Gravity"
+    ]
+  },
+];
